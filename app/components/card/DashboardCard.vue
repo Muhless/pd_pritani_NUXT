ebacc137fa7c205a79e5b92f0b54541bf16900e8
Muhless <nuryadi.muhta@gmail.com>
2025-11-07 10:40:50 +0700

    feat(dashboard): add dashboard card

---
+++ app/components/card/DashboardCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ArrowUpIcon, ArrowUpRightIcon } from "@heroicons/vue/24/solid";
+import type { Component } from "vue";
+
+interface Props {
+  label?: string;
+  content?: string;
+  icon: Component;
+}
+
+const { label, content, icon } = withDefaults(defineProps<Props>(), {
+  label: "Tanpa label",
+  content: "XXX",
+});
+</script>
+
+<template>
+  <div
+    class="bg-white p-3 border border-black border-b-4 rounded-lg shadow-sm text-gray-800"
+  >
+    <div class="w-full flex space-x-3 items-center">
+      <div class="bg-gray-200 rounded-full p-2">
+        <component :is="icon" class="w-5 h-auto" />
+      </div>
+      <h1>{{ label }}</h1>
+    </div>
+    <div class="flex items-center space-x-5 justify-center">
+      <p class="text-3xl py-1 font-bold">{{ content }}</p>
+      <div class="flex text-green-600 space-x-1">
+        <ArrowUpIcon class="w-3 h-auto" />
+        <p class="text-sm">15+</p>
+      </div>
+    </div>
+    <div class="flex justify-between items-center border-t border-black pt-2">
+      <h1 class="text-sm text-green-600 font-bold">
+        +10 <span class="font-normal">Dari bulan kemarin</span>
+      </h1>
+      <div class="bg-gray-200 rounded-full p-2 cursor-pointer">
+        <ArrowUpRightIcon class="w-3 h-auto" />
+      </div>
+    </div>
+  </div>
+</template>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import {
+  ShoppingBagIcon,
+  UserCircleIcon,
+  UsersIcon,
+  ViewColumnsIcon,
+} from "@heroicons/vue/24/solid";
+import BaseCard from "~/components/card/DashboardCard.vue";
 import { useAuthStore } from "~/stores/AuthStores";
 
 useHead({
@@ -13,11 +20,19 @@ definePageMeta({
 
 <template>
   <div>
-    <h1 class="text-2xl font-bold text-green-600 mb-4">Dashboard</h1>
-    <p v-if="auth.user">
-      Selamat datang,
-      <span class="font-semibold">{{ auth.user.username }}</span>
-    </p>
-    <p v-else>Memuat data pengguna...</p>
+    <div class="bg-gray-300 rounded-xl p-2">
+      <h1 class="text-2xl font-bold text-green-600 mb-2">Dashboard</h1>
+      <p v-if="auth.user">
+        Selamat datang,
+        <span class="font-semibold">{{ auth.user.username }}</span>
+      </p>
+      <p v-else>Memuat data pengguna...</p>
+    </div>
+    <div class="space-x-3 grid grid-cols-4">
+      <BaseCard label="Pesanan" content="430" :icon="UserCircleIcon" />
+      <BaseCard label="Produk" :icon="ShoppingBagIcon" />
+      <BaseCard label="Pesanan" :icon="UsersIcon" />
+      <BaseCard label="Pesanan" :icon="ViewColumnsIcon" />
+    </div>
   </div>
 </template>
